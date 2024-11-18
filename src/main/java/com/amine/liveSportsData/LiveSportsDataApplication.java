76ee11ead03b886741dc6a33637972b3e0ba9b8e
Amine <baggaramine1@gmail.com>
2024-11-18 21:43:48 +0100

    the first commit in this project

---
+++ src/main/java/com/amine/liveSportsData/LiveSportsDataApplication.java
@@ -0,0 +1,13 @@
+package com.amine.liveSportsData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveSportsDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveSportsDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amine/liveSportsData/LiveSportsDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amine.liveSportsData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveSportsDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
